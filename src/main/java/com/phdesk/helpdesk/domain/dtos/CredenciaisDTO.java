dc40f1be645061111c869ad75195ad852116dda8
Pedrohswd <pedrohsfwd@gmail.com>
2024-01-26 20:39:16 -0300

    autenticação jwt pronta

--- src/main/java/com/phdesk/helpdesk/domain/dtos/CredenciaisDTO.java
+++ src/main/java/com/phdesk/helpdesk/domain/dtos/CredenciaisDTO.java
@@ -1,4 +1,22 @@
 package com.phdesk.helpdesk.domain.dtos;
 
 public class CredenciaisDTO {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
