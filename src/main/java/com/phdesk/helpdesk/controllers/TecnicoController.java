2e8d0c3dc7db4da8e793881121944eff51897462
Pedro Henrique <pedrohsfwd@gmail.com>
2024-02-02 15:22:32 -0300

    finalizaçao authorization

--- src/main/java/com/phdesk/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/phdesk/helpdesk/controllers/TecnicoController.java
@@ -5,6 +5,7 @@ import com.phdesk.helpdesk.domain.dtos.TecnicoDTO;
 import com.phdesk.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -35,6 +36,7 @@ public class TecnicoController {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
         Tecnico newObj = tecnicoService.create(objDTO);
@@ -42,6 +44,7 @@ public class TecnicoController {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id,@Valid @RequestBody TecnicoDTO tecnicoDTO){
         Tecnico oldObj = tecnicoService.update(id, tecnicoDTO);
@@ -49,6 +52,7 @@ public class TecnicoController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
         tecnicoService.delete(id);
